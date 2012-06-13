1a5b62abd37ef95a6d58cdc682571f32854e32f1
dmcdougall1991@gmail.com <dmcdougall1991@gmail.com@354b1118-4174-9344-3db9-4a8263821fef>
2012-06-13 22:05:28 +0000

    Initial skeleton.

---
+++ OthelloOnline/main.cpp
@@ -0,0 +1,14 @@
+/*
+File: main.cpp
+Project: CSCE315 Project 2
+Authors: Team 3 - Dylan McDougall, Sam Stewart, Stephen Eyck
+Description: This is the main file which takes in user input
+			 and passes it to the othello class. The purpose
+			 is to test the game mechanics.
+*/
+
+#include "othello.h"
+
+int main(){
+
+}
