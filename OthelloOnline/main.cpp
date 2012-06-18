b4da0c3d03ccbdd7535da4a88de1efb3834e8dc7
dmcdougall1991@gmail.com <dmcdougall1991@gmail.com@354b1118-4174-9344-3db9-4a8263821fef>
2012-06-18 23:31:59 +0000

    Easy AI level added.

--- OthelloOnline/main.cpp
+++ OthelloOnline/main.cpp
@@ -15,25 +15,81 @@ Description: This is the main file which takes in user input
 
 int main(){
 	Othello o;
+	AI ai;
 	string input;
 	int rc = 0;
 	cout << "Welcome to the best Othello game eva!\n"
-		 << "Enter a command\n";
 		 
+	//set difficulty
+	bool goAgain = false;
+	do{
+		cout << "\nEnter a difficulty\n>";
+		string diffInput;
+		getline(cin, diffInput);
+		if(diffInput == "EASY"){
+			ai.setDifficulty(EASY);
+			goAgain = false;
+		}
+		else if(diffInput == "MEDIUM"){
+			ai.setDifficulty(MEDIUM);
+			goAgain = false;
+		}
+		else if(diffInput == "HARD"){
+			ai.setDifficulty(HARD);
+			goAgain = false;
+		}
+		else{
+			cout << "Invalid difficulty.\n";
+			goAgain = true;
+		}
+	}while(goAgain);
+	
+	//set color
+	do{
+		cout << "\nEnter your color\n>";
+		string colorInput;
+		getline(cin, colorInput);
+		if(colorInput == "BLACK"){
+			ai.setPlayerColor(WHITE);
+			goAgain = false;
+		}
+		else if(colorInput == "WHITE"){
+			ai.setPlayerColor(BLACK);
+			goAgain = false;
+		}
+		else{
+			cout << "Invalid difficulty.\n";
+			goAgain = true;
+		}
+	}while(goAgain);
+	
+	//play
 	while(rc != 2){
 		//BLACKs turn
 		do{
-			cout << "\nBLACK>";
-			getline(cin, input);
-			rc = o.parse(input, BLACK);
+			if(ai.getColor() == BLACK){
+				cout << "\nBLACK>";
+				rc = o.parse(ai.go(o), BLACK);
+			}
+			else{
+				cout << "\nBLACK>";
+				getline(cin, input);
+				rc = o.parse(input, BLACK);
+			}
 		}while(rc == 0);
 		
 		if(rc != 2){
 			//WHITEs turn
 			do{
-				cout << "\nWHITE>";
-				getline(cin, input);
-				rc = o.parse(input, WHITE);
+				if(ai.getColor() == WHITE){
+					cout << "\nWHITE>";
+					rc = o.parse(ai.go(o), WHITE);
+				}
+				else{
+					cout << "\nWHITE>";
+					getline(cin, input);
+					rc = o.parse(input, WHITE);
+				}
 			}while(rc == 0);
 		}
 	}
