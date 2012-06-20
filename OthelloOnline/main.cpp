e6d9df5bb3f2951f08297e6d17b9d6caa9947350
dmcdougall1991@gmail.com <dmcdougall1991@gmail.com@354b1118-4174-9344-3db9-4a8263821fef>
2012-06-20 21:51:05 +0000

    Changed main to have human play ai.

--- OthelloOnline/main.cpp
+++ OthelloOnline/main.cpp
@@ -20,6 +20,8 @@ int main(){
 	int rc = 0;
 	cout << "Welcome to the best Othello game eva!\n";
 	
+	/*
+	//AI play each other
 	//ai1
 	ai1.setPlayerColor(BLACK);
 	ai1.setDifficulty(MEDIUM);
@@ -27,55 +29,8 @@ int main(){
 	//ai2
 	ai2.setPlayerColor(WHITE);
 	ai2.setDifficulty(HARD);
-		 
-	//set difficulty
-	/*
-	bool goAgain = false;
-	do{
-		cout << "\nEnter a difficulty\n>";
-		string diffInput;
-		getline(cin, diffInput);
-		if(diffInput == "EASY"){
-			ai.setDifficulty(EASY);
-			goAgain = false;
-		}
-		else if(diffInput == "MEDIUM"){
-			ai.setDifficulty(MEDIUM);
-			goAgain = false;
-		}
-		else if(diffInput == "HARD"){
-			ai.setDifficulty(HARD);
-			goAgain = false;
-		}
-		else{
-			cout << "Invalid difficulty.\n";
-			goAgain = true;
-		}
-	}while(goAgain);
 	
-	//set color
-	do{
-		cout << "\nEnter your color\n>";
-		string colorInput;
-		getline(cin, colorInput);
-		if(colorInput == "BLACK"){
-			ai.setPlayerColor(WHITE);
-			ai2.setPlayerColor(BLACK);
-			goAgain = false;
-		}
-		else if(colorInput == "WHITE"){
-			ai.setPlayerColor(BLACK);
-			ai2.setPlayerColor(WHITE);
-			goAgain = false;
-		}
-		else{
-			cout << "Invalid color.\n";
-			goAgain = true;
-		}
-	}while(goAgain);
-	*/
-	
-	//play
+		//play
 	while(rc != 2){
 		//BLACKs turn
 		do{
@@ -105,4 +60,86 @@ int main(){
 			}while(rc == 0);
 		}
 	}
+	*/
+	
+	//You play AI
+	//set difficulty
+	bool goAgain = false;
+	do{
+		cout << "\nEnter a difficulty\n>";
+		string diffInput;
+		getline(cin, diffInput);
+		if(diffInput == "EASY"){
+			ai1.setDifficulty(EASY);
+			goAgain = false;
+		}
+		else if(diffInput == "MEDIUM"){
+			ai1.setDifficulty(MEDIUM);
+			goAgain = false;
+		}
+		else if(diffInput == "HARD"){
+			ai1.setDifficulty(HARD);
+			goAgain = false;
+		}
+		else if(diffInput == "EXPERT"){
+			ai1.setDifficulty(EXPERT);
+			goAgain = false;
+		}
+		else{
+			cout << "Invalid difficulty.\n";
+			goAgain = true;
+		}
+	}while(goAgain);
+	
+	//set color
+	do{
+		cout << "\nEnter your color\n>";
+		string colorInput;
+		getline(cin, colorInput);
+		if(colorInput == "BLACK"){
+			ai1.setPlayerColor(WHITE);
+			goAgain = false;
+		}
+		else if(colorInput == "WHITE"){
+			ai1.setPlayerColor(BLACK);
+			goAgain = false;
+		}
+		else{
+			cout << "Invalid color.\n";
+			goAgain = true;
+		}
+	}while(goAgain);
+	
+		//play
+	while(rc != 2){
+		//BLACKs turn
+		do{
+			if(ai1.getColor() == BLACK){
+				cout << "\nBLACK>";
+				rc = o.parse(ai1.go(o), BLACK);
+			}
+			else{
+				cout << "\nBLACK>";
+				getline(cin, input);
+				rc = o.parse(input, BLACK);
+			}
+		}while(rc == 0);
+		
+		if(rc != 2){
+			//WHITEs turn
+			do{
+				if(ai1.getColor() == WHITE){
+					cout << "\nWHITE>";
+					rc = o.parse(ai1.go(o), WHITE);
+				}
+				else{
+					cout << "\nWHITE>";
+					getline(cin, input);
+					rc = o.parse(input, WHITE);
+				}
+			}while(rc == 0);
+		}
+	}
+	
+
 }
