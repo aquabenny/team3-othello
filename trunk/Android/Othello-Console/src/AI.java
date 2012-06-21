718ddfb352933890b0abb68009dbea78460c6a09
stephen.a.teneyck <stephen.a.teneyck@354b1118-4174-9344-3db9-4a8263821fef>
2012-06-21 05:51:12 +0000

    Added Eclipse project folders, one for the console application, the other for the final android application. Initial priority is to get the Java console application fully functional (minus telnet), then begin building Android on it.

---
+++ trunk/Android/Othello-Console/src/AI.java
@@ -0,0 +1,8 @@
+
+/*
+import android.app.Activity;
+import android.os.Bundle;*/
+
+public class AI extends Othello{
+	//Artificial Intelligence Class
+}
---
+++ trunk/Android/Othello/src/team3/othello/AI.java
@@ -0,0 +1,8 @@
+package team3.othello;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AI extends Othello{
+	//Artificial Intelligence Class
+}
