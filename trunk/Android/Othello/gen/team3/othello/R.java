cd29de3ffb8ecfcd9c981b0096c6693006d7dded
stephen.a.teneyck <stephen.a.teneyck@354b1118-4174-9344-3db9-4a8263821fef>
2012-06-27 05:50:23 +0000

    Added button functionality for the About button and the BEGIN game button. Started attempting to make radiogroup work correctly. Added About and GameBoard activities to the manifest

--- trunk/Android/Othello/gen/team3/othello/R.java
+++ trunk/Android/Othello/gen/team3/othello/R.java
@@ -21,19 +21,20 @@ public final class R {
         public static final int loading_r1=0x7f020002;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f060002;
-        public static final int about_content=0x7f060001;
-        public static final int about_title=0x7f060000;
-        public static final int colorChoose=0x7f060008;
-        public static final int diffChoose=0x7f060003;
-        public static final int radioBlack=0x7f06000a;
-        public static final int radioEasy=0x7f060005;
-        public static final int radioGroup1=0x7f060004;
-        public static final int radioGroup2=0x7f060009;
-        public static final int radioHard=0x7f060007;
-        public static final int radioMed=0x7f060006;
-        public static final int radioWhite=0x7f06000b;
-        public static final int startButton=0x7f06000c;
+        public static final int LinearLayout1=0x7f060001;
+        public static final int about_button=0x7f06000c;
+        public static final int about_content=0x7f060000;
+        public static final int colorChoose=0x7f060007;
+        public static final int colorGroup=0x7f060008;
+        public static final int diffChoose=0x7f060002;
+        public static final int diffGroup=0x7f060003;
+        public static final int highScore=0x7f06000d;
+        public static final int radioBlack=0x7f060009;
+        public static final int radioEasy=0x7f060004;
+        public static final int radioHard=0x7f060006;
+        public static final int radioMed=0x7f060005;
+        public static final int radioWhite=0x7f06000a;
+        public static final int startButton=0x7f06000b;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -57,5 +58,6 @@ public final class R {
         public static final int new_game_label=0x7f050002;
         public static final int quit_label=0x7f05000d;
         public static final int start_label=0x7f05000c;
+        public static final int view_button=0x7f050010;
     }
 }
--- trunk/Android/Othello/src/team3/othello/GameBoard.java
+++ trunk/Android/Othello/src/team3/othello/GameBoard.java
@@ -3,11 +3,31 @@ package team3.othello;
 import team3.othello.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.graphics.*;
 
-public class GameBoard extends Activity {
+public class GameBoard extends Activity implements OnClickListener {
 
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gameboard);
+		
+		//Going to have to define buttons here
+	}
+	
+	public void onClick(View v){
+		/*
+		switch(v.getId()){
+		case R.id.hint_button:
+			break;
+		case R.id.undo_button:
+			break;
+		case R.id.redo_button:
+			break;
+		case R.id.exit_button:
+			finish();
+			break;
+		}*/
 	}
 }
