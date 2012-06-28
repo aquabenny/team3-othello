52cec27ffc44bde8d7ded5433e83764b29523b49
stephen.a.teneyck <stephen.a.teneyck@354b1118-4174-9344-3db9-4a8263821fef>
2012-06-28 21:40:17 +0000

    Started work on actual Game board buttons: Hint, Undo, Redo, Quit. Added HighScores class and activity. Need to find out how to draw...

---
+++ trunk/Android/Othello/src/team3/othello/HighScores.java
@@ -0,0 +1,12 @@
+package team3.othello;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HighScores extends Activity{
+
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.highscores);
+	}
+}
