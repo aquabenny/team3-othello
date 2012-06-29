aba53c4a1bdcbd9340c5c6804279147a7197ad3f
stephen.a.teneyck <stephen.a.teneyck@354b1118-4174-9344-3db9-4a8263821fef>
2012-06-29 05:48:54 +0000

    Added Alerts class, and functionality for error checking with undo/redo. Currently in test mode.

--- Android/Othello/gen/team3/othello/R.java
+++ Android/Othello/gen/team3/othello/R.java
@@ -22,36 +22,40 @@ public final class R {
         public static final int loading_r1=0x7f020002;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f06000b;
-        public static final int about_button=0x7f060016;
+        public static final int LinearLayout1=0x7f06000e;
+        public static final int MSG=0x7f060003;
+        public static final int about_button=0x7f060019;
         public static final int about_content=0x7f060000;
-        public static final int bottom_bar=0x7f060005;
-        public static final int colorChoose=0x7f060011;
-        public static final int colorGroup=0x7f060012;
-        public static final int diffChoose=0x7f06000c;
-        public static final int diffGroup=0x7f06000d;
-        public static final int difficulty=0x7f060002;
-        public static final int gameBoard=0x7f060004;
-        public static final int highScore=0x7f060017;
-        public static final int hint=0x7f060003;
-        public static final int quit=0x7f060007;
-        public static final int radioBlack=0x7f060013;
-        public static final int radioEasy=0x7f06000e;
-        public static final int radioHard=0x7f060010;
-        public static final int radioMed=0x7f06000f;
-        public static final int radioWhite=0x7f060014;
-        public static final int redo=0x7f060008;
-        public static final int startButton=0x7f060015;
-        public static final int textView1=0x7f060009;
-        public static final int top_bar=0x7f060001;
-        public static final int undo=0x7f060006;
-        public static final int xml_scores=0x7f06000a;
+        public static final int bottom_bar=0x7f060008;
+        public static final int colorChoose=0x7f060014;
+        public static final int colorGroup=0x7f060015;
+        public static final int diffChoose=0x7f06000f;
+        public static final int diffGroup=0x7f060010;
+        public static final int difficulty=0x7f060005;
+        public static final int eTitle=0x7f060001;
+        public static final int gameBoard=0x7f060007;
+        public static final int highScore=0x7f06001a;
+        public static final int hint=0x7f060006;
+        public static final int imageView1=0x7f060002;
+        public static final int quit=0x7f06000a;
+        public static final int radioBlack=0x7f060016;
+        public static final int radioEasy=0x7f060011;
+        public static final int radioHard=0x7f060013;
+        public static final int radioMed=0x7f060012;
+        public static final int radioWhite=0x7f060017;
+        public static final int redo=0x7f06000b;
+        public static final int startButton=0x7f060018;
+        public static final int textView1=0x7f06000c;
+        public static final int top_bar=0x7f060004;
+        public static final int undo=0x7f060009;
+        public static final int xml_scores=0x7f06000d;
     }
     public static final class layout {
         public static final int about=0x7f030000;
-        public static final int game=0x7f030001;
-        public static final int highscores=0x7f030002;
-        public static final int main=0x7f030003;
+        public static final int alerts=0x7f030001;
+        public static final int game=0x7f030002;
+        public static final int highscores=0x7f030003;
+        public static final int main=0x7f030004;
     }
     public static final class string {
         public static final int about_content=0x7f05000f;
@@ -64,6 +68,7 @@ public final class R {
         public static final int diff_easy=0x7f050006;
         public static final int diff_hard=0x7f050008;
         public static final int diff_med=0x7f050007;
+        public static final int error_title=0x7f050014;
         public static final int exit_label=0x7f050004;
         public static final int hello=0x7f050000;
         public static final int hint_label=0x7f05000e;
